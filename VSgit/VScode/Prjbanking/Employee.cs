caf4f64faeb1dc9c21a17944cd509f2929e272ec
RamyaVempalli <ramyasatyasrivalli@gmail.com>
2022-03-01 17:22:29 +0530

    Add to git hub

---
+++ VSgit/VScode/Prjbanking/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prjbanking
+{
+    class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public void GetEmployee()
+        {
+
+        }
+        public void DisplayEmployee()
+        {
+
+        }
+    }
+
+}
